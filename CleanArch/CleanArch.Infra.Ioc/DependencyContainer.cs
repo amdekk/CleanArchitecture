e44055b6f6201d4b696f4cb26d0340ac9ae8653d
Yonas <YGashaw@excellerentsolutions.com>
2021-04-19 19:50:56 +0300

    Configure presentation layer for dependency injection.

---
+++ CleanArch/CleanArch.Infra.Ioc/DependencyContainer.cs
@@ -0,0 +1,26 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.Services;
+using CleanArch.Infra.Data.Repository;
+using CLeanArch.Domain.Interface;
+
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Ioc
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Application Layer
+            services.AddScoped<ICourseService, CourseService>();
+
+            //Infra.Data layer
+            services.AddScoped<ICourseRepository, CourseReposity>();
+        }
+    }
+}
