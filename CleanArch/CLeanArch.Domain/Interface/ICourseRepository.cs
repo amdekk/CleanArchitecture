757096e98f53d4398b451d0fa8efa940fffc418a
Yonas <YGashaw@excellerentsolutions.com>
2021-04-19 16:45:11 +0300

    Commit Service and Interfaces, and CourseViewModel

---
+++ CleanArch/CLeanArch.Domain/Interface/ICourseRepository.cs
@@ -0,0 +1,14 @@
+﻿using CLeanArch.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLeanArch.Domain.Interface
+{
+   public interface ICourseRepository
+    {
+        IEnumerable<Course> GetCourses();
+    }
+}
---
+++ CleanArch/CleanArch.Application/ViewModels/CourseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CLeanArch.Domain.Models;
+
+
+namespace CleanArch.Application.ViewModels
+{
+    public class CourseViewModel
+    {
+        public IEnumerable<Course> Courses { get; set; }
+    }
+}
